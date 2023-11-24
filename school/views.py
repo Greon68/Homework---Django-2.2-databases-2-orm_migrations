d63f7987446ff344c3b17a8a6e429ca072817013
Greon68 <greon-ya@yandex.ru>
2023-11-24 19:20:58 +0300

    initial commit

---
+++ school/views.py
@@ -0,0 +1,17 @@
+from django.views.generic import ListView
+from django.shortcuts import render
+
+from .models import Student
+
+
+def students_list(request):
+    student_objects = Student.objects.all()
+    # student_objects = Student.objects.filter(teachers__subject='Физика')
+    template = 'school/students_list.html'
+    context = {'students': student_objects }
+
+    # используйте этот параметр для упорядочивания результатов
+    # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
+    ordering = 'group'
+
+    return render(request, template, context)
